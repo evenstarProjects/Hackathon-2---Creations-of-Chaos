97a03a679a3d19db8bcba9521924603739b52bc0
evenstarProjects <heportrey@gmail.com>
2026-03-11 14:08:24 +0000

    A lot
    Idek how to explain this. A lot happened. Mainly carousel

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+// Opening and Collapsing Dialogue Box
+const helpDialog = document.getElementById("helpDialog");
+const helpButton = document.getElementById("questionMarkButton");
+const closeDialog = document.getElementById("closeDialog");
+
+helpButton.addEventListener("click", () => {
+  helpDialog.showModal();
+});
+
+closeDialog.addEventListener("click", () => {
+  helpDialog.close();
+});
+
 // Rewiring of keys
 const cipher = {
   A: "C", B: "H", C: "A", D: "O", E: "S", F: "B",
