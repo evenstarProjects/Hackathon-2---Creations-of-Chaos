5610e7274a1f499d9136611d2eed1a2a0cef1f91
evenstarProjects <heportrey@gmail.com>
2026-03-11 10:13:35 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Rewiring of keys
+const cipher = {
+  A: "C", B: "H", C: "A", D: "O", E: "S", F: "B",
+  G: "D", H: "E", I: "F", J: "G", K: "I", L: "J",
+  M: "K", N: "L", O: "M", P: "N", Q: "P", R: "Q",
+  S: "R", T: "T", U: "U", V: "V", W: "W", X: "X",
+  Y: "Y", Z: "Z"
+};
+
