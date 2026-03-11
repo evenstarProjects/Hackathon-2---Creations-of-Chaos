f15721143c15d5731428310718c99b0324d4bd6f
evenstarProjects <heportrey@gmail.com>
2026-03-11 15:47:31 +0000

    A chunk completed
    The collapsible help section works and is positioned. Now onto the to do list and other tasks.

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ const helpDialog = document.getElementById("helpDialog");
 const helpButton = document.getElementById("questionMarkButton");
 const closeDialog = document.getElementById("closeDialog");
 
+closeDialog.addEventListener("click", () => {
+  helpDialog.close();
+});
+
 helpButton.addEventListener("click", () => {
   helpDialog.showModal();
 });
 
-closeDialog.addEventListener("click", () => {
-  helpDialog.close();
-});
+
 
 // Rewiring of keys
 const cipher = {
@@ -17,6 +19,11 @@ const cipher = {
   G: "D", H: "E", I: "F", J: "G", K: "I", L: "J",
   M: "K", N: "L", O: "M", P: "N", Q: "P", R: "Q",
   S: "R", T: "T", U: "U", V: "V", W: "W", X: "X",
-  Y: "Y", Z: "Z"
+  Y: "Y", Z: "Z", 
+  a: "c", b: "h", c: "a", d: "o", e: "s", f: "b",
+  g: "d", h: "e", i: "f", j: "g", k: "i", l: "j",
+  m: "k", n: "l", o: "m", p: "n", q: "p", r: "q",
+  s: "r", t: "t", u: "u", v: "v", w: "w", x: "x",
+  y: "y", z: "z"
 };
 
